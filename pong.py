14128bc00ac7573457d73a221503288aaf5256c9
Max Goldstein <maxgoldstein1@gmail.com>
2012-03-07 15:49:35 -0500

    Add opponent score and paddle

--- pong.py
+++ pong.py
@@ -3,8 +3,10 @@ import pygame, sys
 # Constants
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-PADDLE_START_X = 10
-PADDLE_START_Y = 20
+PADDLE1_START_X = 10
+PADDLE1_START_Y = 20
+PADDLE2_START_X = 790
+PADDLE2_START_Y = 20
 PADDLE_WIDTH = 10
 PADDLE_HEIGHT = 100
 BALL_SPEED = 10
@@ -21,12 +23,14 @@ ball_rect = pygame.Rect((SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2), (BALL_WIDTH_HEIGH
 ball_speed = [BALL_SPEED, BALL_SPEED]
 
 # Your paddle vertically centered on the left side
-paddle_rect = pygame.Rect((PADDLE_START_X, PADDLE_START_Y), (PADDLE_WIDTH, PADDLE_HEIGHT))
+paddle1_rect = pygame.Rect((PADDLE1_START_X, PADDLE1_START_Y), (PADDLE_WIDTH, PADDLE_HEIGHT))
+paddle2_rect = pygame.Rect((PADDLE2_START_X, PADDLE2_START_Y), (PADDLE_WIDTH, PADDLE_HEIGHT))
 
 # Scoring: 1 point if you hit the ball, -5 point if you miss the ball
-score = 0
+score1 = 0
+score2 = 0
 
-# Load the font for displaying the score
+# Load the font for displaying the score1
 font = pygame.font.Font(None, 30)
 
 # Game loop
@@ -38,18 +42,18 @@ while True:
 			pygame.quit()
 		# Control the paddle with the mouse
 		elif event.type == pygame.MOUSEMOTION:
-			paddle_rect.centery = event.pos[1]
+			paddle1_rect.centery = event.pos[1]
 			# correct paddle position if it's going out of window
-			if paddle_rect.top < 0:
-				paddle_rect.top = 0
-			elif paddle_rect.bottom >= SCREEN_HEIGHT:
-				paddle_rect.bottom = SCREEN_HEIGHT
+			if paddle1_rect.top < 0:
+				paddle1_rect.top = 0
+			elif paddle1_rect.bottom >= SCREEN_HEIGHT:
+				paddle1_rect.bottom = SCREEN_HEIGHT
 
 	# This test if up or down keys are pressed; if yes, move the paddle
-	if pygame.key.get_pressed()[pygame.K_UP] and paddle_rect.top > 0:
-		paddle_rect.top -= BALL_SPEED
-	elif pygame.key.get_pressed()[pygame.K_DOWN] and paddle_rect.bottom < SCREEN_HEIGHT:
-		paddle_rect.top += BALL_SPEED
+	if pygame.key.get_pressed()[pygame.K_UP] and paddle1_rect.top > 0:
+		paddle1_rect.top -= BALL_SPEED
+	elif pygame.key.get_pressed()[pygame.K_DOWN] and paddle1_rect.bottom < SCREEN_HEIGHT:
+		paddle1_rect.top += BALL_SPEED
 	elif pygame.key.get_pressed()[pygame.K_ESCAPE]:
 		sys.exit(0)
 		pygame.quit()
@@ -65,18 +69,27 @@ while True:
 		ball_speed[0] = -ball_speed[0]
 
 	# Test if the ball is hit by the paddle; if yes reverse speed and add a point
-	if paddle_rect.colliderect(ball_rect):
+	if paddle1_rect.colliderect(ball_rect):
 		ball_speed[0] = -ball_speed[0]
-		score += 1
+		score1 += 1
+
+	if paddle2_rect.colliderect(ball_rect):
+		ball_speed[0] = -ball_speed[0]
+		score2 += 1
 	
 	# Clear screen
 	screen.fill((255, 255, 255))
 
-	# Render the ball, the paddle, and the score
-	pygame.draw.rect(screen, (0, 0, 0), paddle_rect) # Your paddle
+	# Render the ball, the paddle, and the score1
+	pygame.draw.rect(screen, (0, 0, 0), paddle1_rect) # Your paddle
+	pygame.draw.rect(screen, (0, 0, 0), paddle2_rect) # Opponent paddle
 	pygame.draw.circle(screen, (0, 0, 0), ball_rect.center, ball_rect.width / 2) # The ball
-	score_text = font.render(str(score), True, (0, 0, 0))
-	screen.blit(score_text, ((SCREEN_WIDTH / 2) - font.size(str(score))[0] / 2, 5)) # The score
+
+	score1_text = font.render(str(score1), True, (0, 0, 0))
+	screen.blit(score1_text, ((SCREEN_WIDTH / 4) - font.size(str(score1))[0] / 2, 5)) # The score1
+
+	score2_text = font.render(str(score2), True, (0, 0, 0))
+	screen.blit(score2_text, ((SCREEN_WIDTH * 3 / 4) - font.size(str(score2))[0] / 2, 5)) # The score1
 	
 	# Update screen and wait 20 milliseconds
 	pygame.display.flip()
